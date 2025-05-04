4e3f0ad708cdea620a683041444e5005d8b29abb
kokou GBEMAPOU <kokougbemapou@gmail.com>
2025-05-04 18:23:11 +0200

    mise en place du rep. routers

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
 
 const app = express();
 
@@ -8,7 +7,6 @@ const app = express();
 app.use(express.json());
 
 // Middleware pour gérer les CORS
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -21,44 +19,7 @@ mongoose.connect('mongodb+srv://kokou:fcconsulting@cluster0.ibfgfyh.mongodb.net/
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(err => console.error('Erreur MongoDB :', err));
 
-// Route pour récupérer un objet par ID
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
+const stuffRoutes = require('./routes/stuff');
+app.use('/api/stuff', stuffRoutes);
 
-// Route pour récupérer tous les objets
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// Route pour créer un nouvel objet
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// Route pour mettre à jour un objet existant
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// Route pour supprimer un objet
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// Export de l'application
 module.exports = app;
