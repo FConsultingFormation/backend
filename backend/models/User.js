b13ed97ab78466461c0ab0000b11b7d6844ee097
kokou GBEMAPOU <kokougbemapou@gmail.com>
2025-05-10 18:36:54 +0200

    configuration de la partie 3 et 4

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+// Export the model
+module.exports = mongoose.model('User', userSchema);
+
